1bb5d435cec9bbe3db2aa1cb1fd06f4765fa03a8
UgurcanOruc <ugurcan@abisteknoloji.com.tr>
2024-01-08 10:40:25 +0300

    new moderation service added to check if comment has a bad language or so. Client, event bus and comments services updated for that service also.

--- comments/index.js
+++ comments/index.js
@@ -20,7 +20,7 @@ app.post('/posts/:id/comments',async (req, res) => {
 
     const comments = commentsByPostId[req.params.id] || [];
 
-    comments.push({ id: commentId, content });
+    comments.push({ id: commentId, content, status: 'pending' });
     commentsByPostId[req.params.id] = comments;
 
     await axios.post('http://localhost:4005/events', {
@@ -28,7 +28,8 @@ app.post('/posts/:id/comments',async (req, res) => {
         data: {
             id: commentId, 
             content,
-            postId: req.params.id
+            postId: req.params.id,
+            status: 'pending'
         }
     }).catch(error => {
         console.log(error.message);
@@ -37,7 +38,27 @@ app.post('/posts/:id/comments',async (req, res) => {
     res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
+    const { type, data } = req.body;
+
+    if (type === 'CommentModerated') {
+        const { postId, id, status, content } = data;
+        const comments = commentsByPostId[postId];
+
+        const comment = comments.find(comment => comment.id === id);
+        comment.status = status;
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                status,
+                postId,
+                content
+            }
+        }).catch(error => console.log('Failed while sending CommentUpdated request: ', error.message));
+    }
+
     res.send({});
   });
 
--- event-bus/index.js
+++ event-bus/index.js
@@ -1,26 +1,29 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 
-app.use('/events', (req, res) => {
- const event = req.body;
+app.post("/events", (req, res) => {
+  const event = req.body;
 
- axios.post('http://localhost:4000/events', event).catch(error => {
+  axios.post("http://localhost:4000/events", event).catch((error) => {
     console.log(error.message);
-});
- axios.post('http://localhost:4001/events', event).catch(error => {
+  });
+  axios.post("http://localhost:4001/events", event).catch((error) => {
     console.log(error.message);
-});
- axios.post('http://localhost:4002/events', event).catch(error => {
+  });
+  axios.post("http://localhost:4002/events", event).catch((error) => {
     console.log(error.message);
-});
+  });
+  axios.post("http://localhost:4003/events", event).catch((error) => {
+    console.log(error.message);
+  });
 
- res.send({ status: 'OK'});
+  res.send({ status: "OK" });
 });
 
 app.listen(4005, () => {
-    console.log('Listening on 4005.');
-})
+  console.log("Listening on 4005.");
+});
